ad70e52eb075c1edc81c40f1f6ad78c9859fa9b6
Hector Rosas <rosas.h@live.com>
2020-10-31 15:13:13 -0700

    Introduction of new null action to tree root

--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -35,7 +35,9 @@ def setup_behavior_tree():
     spread_action = Action(spread_to_weakest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    marshal_forces = Action(consolidate_ships)
+
+    root.child_nodes = [offensive_plan, marshal_forces.copy(), spread_sequence, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
